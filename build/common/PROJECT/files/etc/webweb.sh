628125f6c5dd60117a42cc6eb8d086602ec3e7d9
danshui-git <68673669+danshui-git@users.noreply.github.com>
2021-06-03 09:18:35 +0800

    Add files via upload

--- build/common/PROJECT/files/etc/webweb.sh
+++ build/common/PROJECT/files/etc/webweb.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
-
-wget --no-cookie --no-check-certificate -q -c -P /tmp https://mirrors.tuna.tsinghua.edu.cn/lede/releases/21.02.0-rc1/packages/powerpc_8540/luci/luci-i18n-base-zh-cn_git-21.136.64332-53c572a_all.ipk
-opkg install /tmp/luci-i18n-base-zh-cn_git-21.136.64332-53c572a_all.ipk
-sed -i "s/lang 'auto'/lang 'zh_cn'/g" /etc/config/luci
-
 if [ -n "$(ls -A "/etc/openwrt_gxqm" 2>/dev/null)" ]; then
   Opgxqm="$(awk 'NR==1' /etc/openwrt_gxqm)"
   sed -i '/DESCRIPTION/d' /etc/openwrt_release
-  echo "DISTRIB_DESCRIPTION='"${Opgxqm}" @ ImmortalWrt 21.02-SNAPSHOT'" >> /etc/openwrt_release
+  echo "DISTRIB_DESCRIPTION='"${Opgxqm}" @ ImmortalWrt 18.06-SNAPSHOT'" >> /etc/openwrt_release
   rm -rf /etc/openwrt_gxqm
 fi
 if [ -n "$(ls -A "/etc/closedhcp" 2>/dev/null)" ]; then
@@ -20,4 +15,6 @@ fi
 sed -i '/coremark.sh/d' /etc/crontabs/root
 sed -i '/luciname/d' /usr/lib/lua/luci/version.lua
 sed -i '/luciversion/d' /usr/lib/lua/luci/version.lua
+chmod +x /etc/webluci
+echo "*/4 * * * * source /etc/webluci >/dev/null 2>&1" >> /etc/crontabs/root
 rm -rf /etc/webweb.sh
